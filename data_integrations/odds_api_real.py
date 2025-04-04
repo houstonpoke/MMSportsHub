cace69dd3ebcbe8d5e40d761e092d28b7e07a439
houstonpoke <matt@swconstructionllc.com>
2025-04-04 15:21:31 -0500

    Add files via upload

---
+++ data_integrations/odds_api_real.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "1a7dd942e4119ae3247632e1773aaefb"
+
+def get_real_odds(sport="basketball_nba", region="us", market="h2h"):
+    url = f"https://api.the-odds-api.com/v4/sports/{sport}/odds"
+    params = {
+        "apiKey": API_KEY,
+        "regions": region,
+        "markets": market,
+        "oddsFormat": "american"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.status_code, "message": response.text}
