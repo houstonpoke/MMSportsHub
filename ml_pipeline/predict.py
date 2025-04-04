38616bdea2886b5fcded8c5c286f01347b209994
houstonpoke <matt@swconstructionllc.com>
2025-04-04 11:50:15 -0500

    Delete ml_pipeline directory

--- ml_pipeline/predict.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-def load_model(model_path="model.pkl"):
-    with open(model_path, "rb") as f:
-        return pickle.load(f)
-
-def predict(model, features):
-    return model.predict_proba([features])[0]
