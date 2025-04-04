8a73340aff5d6bb1ba2eaf0b816d6de36767ca6e
houstonpoke <matt@swconstructionllc.com>
2025-04-04 11:23:10 -0500

    Delete features directory

--- features/features.py
+++
@@ -1,7 +0,0 @@
-def build_features(game_stats):
-    # Placeholder for building features from game stats
-    return [
-        game_stats.get("team_a_score", 0) - game_stats.get("team_b_score", 0),
-        game_stats.get("team_a_yards", 0),
-        game_stats.get("team_b_yards", 0),
-    ]
