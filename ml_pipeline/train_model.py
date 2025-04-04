14a6a34b2936e9ab39e0f0421ded3096808b7255
houstonpoke <matt@swconstructionllc.com>
2025-04-04 11:23:16 -0500

    Delete ml_pipeline directory

--- ml_pipeline/train_model.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import pickle
-
-def train_model(X, y, save_path="model.pkl"):
-    model = LogisticRegression()
-    model.fit(X, y)
-    with open(save_path, "wb") as f:
-        pickle.dump(model, f)
