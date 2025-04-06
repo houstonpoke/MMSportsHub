c0e1917621e4b6d2f758f1aba89ce5d21ad4cd28
houstonpoke <matt@swconstructionllc.com>
2025-04-05 20:58:48 -0500

    Add files via upload

--- predictive_models/model_manager.py
+++ predictive_models/model_manager.py
@@ -1,15 +1,23 @@
 ELO_RATINGS = {
+    "Lakers": 1600,
+    "Celtics": 1650,
     "Florida": 1655,
     "Auburn": 1640,
+    "Duke": 1685,
     "Houston": 1700,
-    "Duke": 1685
+    "Alabama": 1620,
+    "UConn": 1750
 }
 
 TEAM_ALIASES = {
+    "Los Angeles Lakers": "Lakers",
+    "Boston Celtics": "Celtics",
     "Florida Gators": "Florida",
     "Auburn Tigers": "Auburn",
+    "Duke Blue Devils": "Duke",
     "Houston Cougars": "Houston",
-    "Duke Blue Devils": "Duke"
+    "Alabama Crimson Tide": "Alabama",
+    "Connecticut Huskies": "UConn"
 }
 
 def get_elo(team):
@@ -19,7 +27,14 @@ def get_elo(team):
 def win_probability(team_a, team_b):
     rating_a = get_elo(team_a)
     rating_b = get_elo(team_b)
+
+    # Elo-based expected probability
     elo_prob = 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
-    logistic_prob = 1 / (1 + 10 ** ((rating_b - rating_a) / 300))  # simulate model adjustment
+
+    # Simulated logistic regression layer
+    logistic_prob = 1 / (1 + 10 ** ((rating_b - rating_a) / 300))
+
+    # Combined model prediction
     final_prob = round((elo_prob + logistic_prob) / 2, 3)
+
     return final_prob, rating_a, rating_b
